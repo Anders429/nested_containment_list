4445702b9b7bf6859b7937243a570ad6af794faf
Anders429 <anders.stefan.evensen@gmail.com>
2021-02-05 08:50:38 -0800

    Add ordering for NestedContainment.

--- src/lib.rs
+++ src/lib.rs
@@ -111,11 +111,26 @@ use core::option;
 
 mod impls;
 
-trait NestedContainment<R, T> where R: RangeBounds<T> {
-    fn contains<S>(&self, inner: &S) -> bool where S: RangeBounds<T>;
+trait NestedContainment<R, T>
+where
+    R: RangeBounds<T>,
+{
+    fn contains<S>(&self, inner: &S) -> bool
+    where
+        S: RangeBounds<T>;
+    fn ordering<S>(&self, other: &S) -> Ordering
+    where
+        S: RangeBounds<T>;
+    fn end_bound_ordering<S>(&self, other: &S) -> Ordering
+    where
+        S: RangeBounds<T>;
 }
 
-impl<R, T> NestedContainment<R, T> for R where R: RangeBounds<T>, T: PartialOrd<T> {
+impl<R, T> NestedContainment<R, T> for R
+where
+    R: RangeBounds<T>,
+    T: PartialOrd<T>,
+{
     fn contains<S>(&self, inner: &S) -> bool
     where
         S: RangeBounds<T>,
@@ -145,6 +160,107 @@ impl<R, T> NestedContainment<R, T> for R where R: RangeBounds<T>, T: PartialOrd<
             },
             Bound::Unbounded => true,
         })
+    }
+
+    fn ordering<S>(&self, other: &S) -> Ordering
+    where
+        S: RangeBounds<T>,
+    {
+        match self.start_bound() {
+            Bound::Included(self_start) => match other.start_bound() {
+                Bound::Included(other_start) => {
+                    if self_start < other_start {
+                        Ordering::Less
+                    } else if self_start == other_start {
+                        self.end_bound_ordering(other)
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Excluded(other_start) => {
+                    if self_start <= other_start {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Unbounded => Ordering::Greater,
+            },
+            Bound::Excluded(self_start) => match other.start_bound() {
+                Bound::Included(other_start) => {
+                    if self_start < other_start {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Excluded(other_start) => {
+                    if self_start < other_start {
+                        Ordering::Less
+                    } else if self_start == other_start {
+                        self.end_bound_ordering(other)
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Unbounded => Ordering::Greater,
+            },
+            Bound::Unbounded => match other.start_bound() {
+                Bound::Unbounded => Ordering::Equal,
+                _ => Ordering::Less,
+            },
+        }
+    }
+
+    #[inline]
+    fn end_bound_ordering<S>(&self, other: &S) -> Ordering
+    where
+        S: RangeBounds<T>,
+    {
+        match self.end_bound() {
+            Bound::Included(self_end) => match other.end_bound() {
+                Bound::Included(other_end) => {
+                    if self_end > other_end {
+                        Ordering::Less
+                    } else if self_end == other_end {
+                        Ordering::Equal
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Excluded(other_end) => {
+                    if self_end >= other_end {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Unbounded => Ordering::Less,
+            },
+            Bound::Excluded(self_end) => match other.end_bound() {
+                Bound::Included(other_end) => {
+                    if self_end > other_end {
+                        Ordering::Less
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Excluded(other_end) => {
+                    if self_end > other_end {
+                        Ordering::Less
+                    } else if self_end == other_end {
+                        Ordering::Equal
+                    } else {
+                        Ordering::Greater
+                    }
+                }
+                Bound::Unbounded => Ordering::Less,
+            },
+            Bound::Unbounded => match other.end_bound() {
+                Bound::Unbounded => Ordering::Equal,
+                _ => Ordering::Greater,
+            },
+        }
     }
 }
 
